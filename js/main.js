7780f5640db5cedb07a7a495761f75f99ef1b782
csomosdm <csomosdm@rose-hulman.edu>
2022-10-28 14:45:41 -0400

    added main.js with Game class

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var rhit = rhit || {};
+
+rhit.Game = class {
+    constructor() {
+        //Instance variables
+        this.level = null;
+        this.totalNotes = 0;
+        this.perfectNotes = 0;
+        this.greatNotes = 0;
+        this.goodNotes = 0;
+        this.score = 0;
+        this.noteStreak = 0;
+    }
+
+    updateScore() {
+
+    }
+
+    begin() {
+
+    }
+
+    pause() {
+
+    }
+
+    checkForKeyPressed() {
+
+    }
+
+    postToLeaderboard() {
+
+    }
+
+    postToPersonalStats() {
+
+    }
+}
+
+rhit.main = function () {
+	console.log("Ready");
+};
+
+rhit.main();
